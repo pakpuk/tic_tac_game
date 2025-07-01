fba5eaed18859c8a9e9d8bf0a091b7d4663c055e
Ghaith <ghayth.1900@gmail.com>
2025-07-01 18:30:06 +0200

    finish first setups

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.blue,
+      ),
       home: HomeScreen(),
     );
   }
---
+++ lib/theme/color_manager.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ColorManager {
+  static const primaryColor = Color(0xFF00061a);
+  static const shadowColor = Color(0xFF001456);
+  static const splashColor = Color(0xFF4169e8);
+}
