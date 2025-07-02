b1d31abdb8164b01b8ebe7ad0e0012fedb6f1fc1
Ghaith <ghayth.1900@gmail.com>
2025-07-02 09:32:19 +0200

    start working on  game logic

--- lib/view/screens/home_screen.dart
+++ lib/view/screens/home_screen.dart
@@ -55,15 +55,26 @@ class _HomeScreenState extends State<HomeScreen> {
             textAlign: TextAlign.center,
           ),
           Expanded(
-            child: GridView.count(crossAxisCount: 3,shrinkWrap: true,
-             padding:  const  EdgeInsets.all(16),
+            child: GridView.count(
+              padding: const EdgeInsets.all(16),
               mainAxisSpacing: 0,
-              crossAxisSpacing: 0,  
+              crossAxisSpacing: 0,
               childAspectRatio: 1.0,
-              children: List.generate(9,(index)=>),
+              crossAxisCount: 3,
+              children: List.generate(
+                  9,
+                  (index) => InkWell(
+                        onTap: () {},
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: ColorManager.shadowColor,
+                            borderRadius: BorderRadius.circular(16),
+                          ),
+                          child: Text("data"),
+                        ),
+                      )),
             ),
           ),
-
           SizedBox(height: 8),
           ElevatedButton.icon(
             onPressed: () {},
